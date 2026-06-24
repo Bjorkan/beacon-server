4860921e90fa2f35a5cfde6d2c36c57bcddc46b9
Enot (ded) Skelly <enot@mailbox.org>
2026-06-24 08:04:54 -0700

    chore: version bump v1.5.4

--- docs/docs.go
+++ docs/docs.go
@@ -3533,7 +3533,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.5.3",
+	Version:          "1.5.4",
 	Host:             "localhost:8080",
 	BasePath:         "/api/v1",
 	Schemes:          []string{"http", "https"},
